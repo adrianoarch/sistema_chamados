0481b62e3d8db7be3f881d4c0f5f4867aba36454
Adriano de Oliveira <adriano.98@gmail.com>
2022-07-23 21:48:29 -0300

    Adicionando Route users

--- app_chamados/routes/web.php
+++ app_chamados/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\{
+    UserController,
+    HomeController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,6 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
