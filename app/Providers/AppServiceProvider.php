90ec9b48a5232a6849d25b07d3732f16fbcb8361
Adriano de Oliveira <adriano.oliveira@facedigital.com.br>
2023-03-18 18:58:59 -0300

    Validação emails de usuarios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,9 @@ use Illuminate\Support\ServiceProvider;
 // use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Channels\MailChannel;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Validator;
 class AppServiceProvider extends ServiceProvider
+
 {
     /**
      * Register any application services.
@@ -25,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // 
+        Validator::extend('valid_email_domain', function ($attribute, $value, $parameters, $validator) {
+            $valid_domains = ['gmail.com', 'hotmail.com', 'outlook.com', 'yahoo.com', 'sudesb.ba.gov.br'];
+            $domain = explode('.', $value)[1];
+            return in_array($domain, $valid_domains);
+        }); 
     }
 }
